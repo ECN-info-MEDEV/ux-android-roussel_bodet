d00e37d2245cbe634b2af3188f2bd5993e7b2f0b
alan <alanroussel@free.fr>
2022-03-17 11:25:16 +0100

    login and register page, need to merge

--- ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/RegisterActivity.java
+++ ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/RegisterActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
-    public void openRegisterPage(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
+    public void openLoginPage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }

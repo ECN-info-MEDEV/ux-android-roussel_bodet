e982a825bd701227db6aa7c2801bce052bf796a8
AlexandreBodet <bodetalexandre1999@gmail.com>
2022-03-24 09:04:25 +0100

    settings page

---
+++ ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.applicationbanque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void openWebsiteCA(View view) {
+        String url = getString(R.string.linkCA);
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+    }
+
+}
--- ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/SummaryActivity.java
+++ ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/SummaryActivity.java
@@ -2,13 +2,20 @@ package com.example.android.applicationbanque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Summary extends AppCompatActivity {
+public class SummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+    }
+
+    public void openSettingsPage(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }

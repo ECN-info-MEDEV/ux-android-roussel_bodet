939da51e90a84bccd8fd87a23154abbdce59c812
alan <alanroussel@free.fr>
2022-03-24 09:04:37 +0100

    login to summary and reverse

--- ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/Summary.java
+++ ApplicationBanque/app/src/main/java/com/example/android/applicationbanque/Summary.java
@@ -2,7 +2,10 @@ package com.example.android.applicationbanque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.username_text);
+        textView.setText(message);
+    }
+
+    public void LogOut(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
